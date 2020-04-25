38eb3b56546b2b13501ab3c5425a0f04db8cc4c5
choudharymahipal <mschoudhary81@gmail.com>
2020-04-25 12:36:17 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,21 +12,15 @@ export class AppComponent {
 }
 
 export interface PeriodicElement {
+  id:number;
   name: string;
-  position: number;
-  weight: number;
-  symbol: string;
+  Designation: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: "Hydrogen", weight: 1.0079, symbol: "H" },
-  { position: 2, name: "Helium", weight: 4.0026, symbol: "He" },
-  { position: 3, name: "Lithium", weight: 6.941, symbol: "Li" },
-  { position: 4, name: "Beryllium", weight: 9.0122, symbol: "Be" },
-  { position: 5, name: "Boron", weight: 10.811, symbol: "B" },
-  { position: 6, name: "Carbon", weight: 12.0107, symbol: "C" },
-  { position: 7, name: "Nitrogen", weight: 14.0067, symbol: "N" },
-  { position: 8, name: "Oxygen", weight: 15.9994, symbol: "O" },
-  { position: 9, name: "Fluorine", weight: 18.9984, symbol: "F" },
-  { position: 10, name: "Neon", weight: 20.1797, symbol: "Ne" }
+  { id: 1, name: "Manoj Puri", Designation: "Team Leader"},
+  { id: 2, name: "Mahipal Choudhary", Designation: "Developer"},
+  { id: 3, name: "Meghna Kumari", Designation: "Developer"},
+  { id: 4, name: "Manoj jangid", Designation: "Developer"},
+  { id: 5, name: "Praveen Rathore", Designation: "Developer"}
 ];
